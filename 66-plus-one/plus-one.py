4811090f3085752fca1eaa9344bcf4fcaed8762f
KHALILULLAH AL FAATH <khalilullah.alfaath21@gmail.com>
2025-03-06 15:19:42 +0800

    Time: 3 ms (9.43%) | Memory: 12.4 MB (80.55%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -4,32 +4,20 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        # index = len(digits)
-        # total = 0
+        index = len(digits)
+        total = 0
 
-        # # get the total value
-        # for i in range(len(digits)): 
-        #     total = total + (digits[i] * (10**(index-1)))
-        #     index -= 1
+        # get the total value
+        for i in range(len(digits)): 
+            total = total + (digits[i] * (10**(index-1)))
+            index -= 1
 
-        # # add one
-        # total = total + 1
+        # add one
+        total = total + 1
 
-        # res = [int(digit) for digit in str(total)]
+        res = [int(digit) for digit in str(total)]
 
-        # return res
-
-        n = len(digits)
-
-        # Iterate from the last digit to the first
-        for i in range(n - 1, -1, -1):
-            if digits[i] < 9:
-                digits[i] += 1
-                return digits  # No carry, return early
-            digits[i] = 0  # If it's 9, set to 0 and continue
-
-        # If all digits were 9, we need to add a new digit at the beginning
-        return [1] + digits
+        return res
 
 
 
