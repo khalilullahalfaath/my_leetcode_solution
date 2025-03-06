38374d8f76e08e2de9968c59967792c7c830fc0c
KHALILULLAH AL FAATH <khalilullah.alfaath21@gmail.com>
2025-03-06 15:42:50 +0800

    Time: 10 ms (19.79%) | Memory: 23.6 MB (11.36%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -5,4 +5,20 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        return int(np.sqrt(x))
+        if x == 0:
+            return 0
+        
+        # Initial guess (can start with x or x/2 for faster convergence)
+        approx = x  
+
+        # Iterate using Heron's method until the approximation is stable
+        while True:
+            better_approx = 0.5 * (approx + x / approx)
+            
+            # Stop when the difference is very small
+            if abs(better_approx - approx) < 1e-6:
+                break
+            
+            approx = better_approx
+        
+        return int(approx)  # Return the integer part as required
